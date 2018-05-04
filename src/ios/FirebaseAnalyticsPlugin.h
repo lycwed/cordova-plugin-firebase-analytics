88933fca31a7abf4c4ac328924874ba48409fe1c
lycwed dev <ylechevalier@gmail.com>
2018-05-04 10:06:15 +0200

    fix compilation bug

--- src/ios/FirebaseAnalyticsPlugin.h
+++ src/ios/FirebaseAnalyticsPlugin.h
@@ -2,10 +2,10 @@
 
 @interface FirebaseAnalyticsPlugin : CDVPlugin
 
-- (void)logEvent:(CDVInvokedUrlCommand *)command;
-- (void)setUserId:(CDVInvokedUrlCommand *)command;
-- (void)setUserProperty:(CDVInvokedUrlCommand *)command;
-- (void)setEnabled:(CDVInvokedUrlCommand *)command;
-- (void)setCurrentScreen:(CDVInvokedUrlCommand *)command;
+- (void)logEvent:(CDVInvokedUrlCommand*)command;
+- (void)setUserId:(CDVInvokedUrlCommand*)command;
+- (void)setUserProperty:(CDVInvokedUrlCommand*)command;
+- (void)setEnabled:(CDVInvokedUrlCommand*)command;
+- (void)setCurrentScreen:(CDVInvokedUrlCommand*)command;
 
 @end
